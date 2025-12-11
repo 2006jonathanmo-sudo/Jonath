abcd636e6f0d27d4dfb1ec8bd5ff89360389383a
2006jonathanmo-sudo <2006.jonathan.m.o@gmail.com>
2025-12-11 00:45:58 -0600

    Update lover name in final message

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ async function handleFormarCorazon() {
     await animateHeartTerminal();
     
     // Show final message
-    loverNameSpan.textContent = ',María alexa ! ♥';
+    loverNameSpan.textContent = ',María ! ♥';
     finalMessage.classList.remove('hidden');
     
     isAnimating = false;
